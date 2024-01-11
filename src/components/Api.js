b2f7ab2abca4a0772e3837e6be0606f3cf9ad0a9
Luke Smeenge <Luke170607@gmail.com>
2024-01-10 18:50:11 -0800

    More updates to Sprint 9

--- src/components/Api.js
+++ src/components/Api.js
@@ -10,26 +10,26 @@ export class Api {
     if (res.ok) {
       return res.json();
     }
-    // if the server returns an error, reject the promise
+    
     return Promise.reject(`Error: ${res.status}`);
   }
-  //load cards from server
+  
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
     }).then(this._handleResponse);
   }
-  // refactor duplicate code i.e authorization and fetch
+  
   getCardsById(id) {
     return fetch(`${this._baseUrl}/cards/${id}`);
   }
-  //load user info from server
+  /
   loadInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
     }).then(this._handleResponse);
   }
-  //editing profile
+  
   updateInfo(modalInputs) {
     const { title, description } = modalInputs;
     console.log(modalInputs);
@@ -56,10 +56,10 @@ export class Api {
       this._handleResponse(res);
     });
   }
-  //Post new card to Api
+
   postCards(modalInputs) {
     const { place, Url } = modalInputs;
-    //let id;
+    
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: this._headers,
@@ -72,7 +72,7 @@ export class Api {
     });
   }
   deleteCards(id) {
-    /// grab id from card-send id to this api call-delete card from html
+    
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
       headers: this._headers,
@@ -81,7 +81,6 @@ export class Api {
     });
   }
   notLiked(id) {
-    /// grab id from card-send id to this api toggle like
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "DELETE",
       headers: this._headers,
@@ -90,7 +89,7 @@ export class Api {
     });
   }
   toLike(id) {
-    /// grab id from card-send id to this api call toggle like
+    
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "PUT",
       headers: this._headers,
